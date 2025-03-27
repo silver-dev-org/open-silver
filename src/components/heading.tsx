93003342b58a5aa413f4e0d4ca85930db150860e
Nicolás Pujia <nicopujia.dev@gmail.com>
2025-03-27 18:47:11 -0300

    base ui

---
+++ src/components/heading.tsx
@@ -0,0 +1,18 @@
+export default function Heading({
+  description,
+  children,
+  center = true,
+}: {
+  description: string;
+  children: React.ReactNode;
+  center?: boolean;
+}) {
+  return (
+    <div className={`space-y-8 ${center ? "text-center" : "text-left"}`}>
+      <h1 className="text-4xl sm:text-6xl lg:text-8xl font-serif text-balance pt-32">
+        {children}
+      </h1>
+      <p className="text-xl">{description}</p>
+    </div>
+  );
+}
