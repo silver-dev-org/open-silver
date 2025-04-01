bb877e5f868991fe64c6a747358326ef3a6757bc
Nicolás Pujia <nicopujia.dev@gmail.com>
2025-04-01 17:56:51 -0300

    add more questions

--- src/behavioral-checker/data/questions.ts
+++ src/behavioral-checker/data/questions.ts
@@ -14,7 +14,38 @@ export const questions: Question[] = [
   },
   {
     id: "about-yourself",
-    text: "Tell me about yourself",
+    text: "Tell me a bit about yourself.",
     maxTime: 30,
+  },
+  {
+    id: "company-values",
+    text: "We care a lot about this value. Can you tell me a time where you demonstrated it?",
+    tags: ["value"],
+    maxTime: 45,
+  },
+  {
+    id: "disagreement-with-leadership",
+    text: "Sometimes there are disagreements with leadership. Can you give me an example of a time when you had disagreements with your manager or company leadership, and what did you do?",
+    maxTime: 45,
+  },
+  {
+    id: "handling-underperformance",
+    text: "If a colleague or direct report is not performing according to expectations, and this starts negatively impacting the company roadmap or teamwork, how would you handle this situation?",
+    maxTime: 45,
+  },
+  {
+    id: "biggest-technical-challenge",
+    text: "What was the most difficult technical challenge you had to face?",
+    maxTime: 45,
+  },
+  {
+    id: "failed-project",
+    text: "Tell me about a project of yours that failed.",
+    maxTime: 45,
+  },
+  {
+    id: "questions-for-interviewer",
+    text: "Do you have any questions for us?",
+    maxTime: 15,
   },
 ];
