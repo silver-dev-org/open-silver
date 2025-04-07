968c80aaf1fda6cf895124d4bc723b66246cce38
Nicolás Pujia <nicopujia.dev@gmail.com>
2025-04-07 18:21:13 -0300

    save scores on cookie

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,35 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export class PreppingData {
+  static readonly cookieName = "prepping-data";
+  static readonly tools = {
+    "behavioral-checker": "b",
+    "take-home-checker": "t",
+    "resume-checker": "r",
+  };
+
+  static getCookieData() {
+    return JSON.parse(
+      decodeURIComponent(
+        document.cookie
+          .split("; ")
+          .find((row) => row.startsWith(this.cookieName))
+          ?.split("=")[1] || "{}"
+      )
+    );
+  }
+
+  static getToolData(tool: keyof typeof this.tools) {
+    return this.getCookieData()[this.tools[tool]] || {};
+  }
+
+  static setToolData(tool: keyof typeof this.tools, value: Object | string) {
+    const domain =
+      typeof window !== "undefined" ? window.location.hostname : "localhost";
+    const data = this.getCookieData();
+    data[this.tools[tool]] = value;
+    document.cookie = `${this.cookieName}=${encodeURIComponent(JSON.stringify(data))}; domain=${domain}; path=/; max-age=31536000; samesite=none; secure`;
+  }
+}
