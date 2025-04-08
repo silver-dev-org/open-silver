214909edb6647fcd7bb7319f51e7961bb7788224
Nicolás Pujia <nicopujia.dev@gmail.com>
2025-04-08 17:59:46 -0300

    configure stt language
    always english

--- src/behavioral-checker/client-assistance/infrastructure/OpenAISdkSTTClient.ts
+++ src/behavioral-checker/client-assistance/infrastructure/OpenAISdkSTTClient.ts
@@ -1,27 +1,26 @@
+import fs from "fs";
 import OpenAI from "openai";
-import fs from 'fs';
-import path from 'path';
+import path from "path";
 import { STTClient } from "../core/domain/STTClient";
 
 export class OpenAISdkSTTSClient implements STTClient {
+  private client: OpenAI;
 
-    private client: OpenAI;
+  constructor() {
+    this.client = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+  }
 
-    constructor() {
-        this.client = new OpenAI({
-            apiKey: process.env.OPENAI_API_KEY,
-        });
-    }
+  async transcribeAudio(p: string): Promise<string> {
+    const audioPath = path.resolve(p);
 
-    async transcribeAudio(p: string): Promise<string> {
-        const audioPath = path.resolve(p);
+    const response = await this.client.audio.transcriptions.create({
+      file: fs.createReadStream(audioPath),
+      model: "whisper-1",
+      language: "en",
+    });
 
-        const response = await this.client.audio.transcriptions.create({
-            file: fs.createReadStream(audioPath),
-            model: 'whisper-1',
-        });
-
-        return response.text;
-    }
-
+    return response.text;
+  }
 }
