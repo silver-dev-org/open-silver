4605a525ac242c07dfb9e707c4dae4ae9002716c
Nicolás Pujia <nicopujia.dev@gmail.com>
2025-04-10 16:17:07 -0300

    add minimum time for questions

--- src/behavioral-checker/data/questions.ts
+++ src/behavioral-checker/data/questions.ts
@@ -2,6 +2,7 @@ export type Question = {
   id: string;
   text: string;
   tags?: string[];
+  minTime: number;
   maxTime: number;
 };
 
@@ -10,45 +11,53 @@ export const questions: Question[] = [
     id: "interest-in-position",
     text: "Why are you interested in this position?",
     tags: ["company", "role"],
+    minTime: 10,
     maxTime: 90,
   },
   {
     id: "about-yourself",
     text: "Tell me a bit about yourself.",
+    minTime: 10,
     maxTime: 90,
   },
   {
     id: "company-values",
     text: "We care a lot about this value. Can you tell me a time when you demonstrated it?",
     tags: ["value"],
+    minTime: 20,
     maxTime: 120,
   },
   {
     id: "disagreement-with-leadership",
     text: "How do you handle disagreements with your manager or company leadership?",
+    minTime: 20,
     maxTime: 120,
   },
   {
     id: "handling-underperformance",
     text: "How do you handle underperformance in your team?",
+    minTime: 20,
     maxTime: 120,
   },
   {
     id: "biggest-technical-challenge",
     text: "What was the most difficult technical challenge you had to face?",
     tags: ["role"],
+    minTime: 20,
     maxTime: 120,
   },
   {
     id: "failed-project",
     text: "Tell me about a project of yours that failed.",
     tags: ["role"],
+    minTime: 20,
     maxTime: 120,
   },
   {
     id: "questions-for-interviewer",
     text: "Do you have any questions for us?",
     tags: ["company"],
+    minTime: 5,
     maxTime: 30,
   },
 ];
