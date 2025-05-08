e4363ce00c667a8676485a4fd3dc6a75664a80b0
Nicolás Pujia <nicopujia.dev@gmail.com>
2025-05-08 15:16:50 -0300

    add company logos to testimonials

--- src/app/mentors/page.tsx
+++ src/app/mentors/page.tsx
@@ -2,6 +2,7 @@ import Description from "@/components/description";
 import Divider from "@/components/divider";
 import Grid from "@/components/grid";
 import Heading from "@/components/heading";
+import { Aleph, Ebay, Gerald, LinkedIn } from "@/components/logos";
 import Section from "@/components/section";
 import Spacer from "@/components/spacer";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -29,7 +30,6 @@ import {
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { SVGProps } from "react";
 
 const benefits = [
   {
@@ -73,6 +73,7 @@ const testimonials = [
       "Con Marcel logré sentirme seguro a la hora de interactuar en entrevistas. Pudo entender lo que necesitaba aprender para lograr mis objetivos profesionales y fue clave para poder transmitir mi forma de ser y expresarme de forma genuina.",
     link: "https://www.linkedin.com/in/matiassaguir",
     photo: "/mentors/testimonials/matias.jpeg",
+    company: Gerald,
   },
   {
     name: "Martín Sione",
@@ -81,6 +82,7 @@ const testimonials = [
       "La experiencia de Gabriel en empresas americanas de producto me ayudó a saber qué es lo que tenía que destacar de mi perfil. Es muy valioso tener a alguien que realmente trabajó en la industria y participó de procesos de hiring.",
     link: "https://www.linkedin.com/in/martinsione/",
     photo: "/mentors/testimonials/martin.jpeg",
+    company: Aleph,
   },
   {
     name: "Francisco Erramuspe",
@@ -89,6 +91,7 @@ const testimonials = [
       "Gracias a la mentoría con Mariano pude conseguir ofertas de Walmart, eBay y Google. Me ayudó mucho a mejorar mis problem solving skills de cara a las entrevistas técnicas así como a manejar situaciones de ambigüedad y presión. No podría haber logrado esos resultados sin él: su ayuda fue fundamental para pasar muchas rondas de entrevista.",
     link: "https://www.linkedin.com/in/francisco-erramuspe/",
     photo: "/mentors/testimonials/francisco.jpeg",
+    company: Ebay,
   },
 ];
 
@@ -246,7 +249,7 @@ export default function MentorsPage() {
             {testimonials.map((testimonial, index) => (
               <CarouselItem
                 key={index}
-                className="basis-10/12 sm:basis-4/5 md:basis-3/5 lg:basis-2/5 xl:basis-1/5"
+                className="basis-10/12 sm:basis-4/5 md:basis-3/5 lg:basis-2/5 xl:basis-1/4"
               >
                 <Card className="h-full flex flex-col">
                   <CardHeader>
@@ -286,6 +289,7 @@ export default function MentorsPage() {
                           </p>
                         </div>
                       </div>
+                      <testimonial.company className="fill-foreground w-16" />
                     </Link>
                   </CardFooter>
                 </Card>
@@ -352,13 +356,5 @@ export default function MentorsPage() {
         </Grid>
       </Section>
     </>
-  );
-}
-
-function LinkedIn({ ...props }: SVGProps<SVGSVGElement>) {
-  return (
-    <svg {...props} aria-label="LinkedIn" role="img" viewBox="0 0 16 16">
-      <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
-    </svg>
   );
 }
