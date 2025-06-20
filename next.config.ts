ebb79329584cfdbe1c81ed181c66cd2cb039981e
Nicolás Pujia <nicopujia.dev@gmail.com>
2025-06-20 15:26:21 -0300

    replace mentors page with redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: "/mentors",
+        destination: "https://silver.dev/mentors",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
