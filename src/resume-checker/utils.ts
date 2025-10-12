48ca56968c306b8ea9f48426034730d59a8ebd8a
Leandro Linardos <leandro.linardos@gmail.com>
2025-10-12 20:14:09 -0300

    fix: update typst template url & use it where harcoded

--- src/resume-checker/utils.ts
+++ src/resume-checker/utils.ts
@@ -1,2 +1,2 @@
 const TYPST_TEMPLATE_VERSION = "1.0.2";
-export const TYPST_TEMPLATE_URL = `https://typst.app/app?template=silver-dev-cv&version=${TYPST_TEMPLATE_VERSION}`;
+export const TYPST_TEMPLATE_URL = `https://typst.app/app?template=silver-dev-cv&q=silver-dev-cv&version=${TYPST_TEMPLATE_VERSION}`;
