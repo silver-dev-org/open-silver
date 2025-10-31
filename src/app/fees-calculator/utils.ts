a69c136acac7924456e323ad6ab8a9cff1d33e7d
Nicolás Pujia <nicopujia.dev@gmail.com>
2025-10-31 15:58:33 -0300

    refactor fees calc

--- src/app/fees-calculator/utils.ts
+++ src/app/fees-calculator/utils.ts
@@ -1,23 +1,36 @@
-export const payrollCost = 500;
-export const feeWithFastProcessing = 20;
-
-// Using single letters to keep the URL shorter
 export interface ContractProps {
+  // Using single letters to keep the URL shorter
   n: number; // Number of hires
   f: number; // Fee
   s: number; // Salary
-  h: boolean; //
-  x: boolean; // Exclusivity
   p: boolean; // Payroll
-  d: boolean; // Deferred payment
-  g: boolean; // Pay as you go
-  t: boolean; // Strong guarantee
   fp: boolean; // Fast processing
   [key: string]: any;
 }
 
+export const defaultFee = 25;
+export const fastProcessingFee = 20;
+export const payrollCost = 500;
+export const headcountOptions = [
+  { value: "1", label: "1" },
+  { value: "2", label: "2" },
+  { value: "3", label: "3+" },
+];
+export const salaryOptions = [
+  { value: "50000", label: "$50k" },
+  { value: "75000", label: "$75k" },
+  { value: "100000", label: "$100k+" },
+];
+export const defaultContractProps: ContractProps = {
+  n: parseInt(headcountOptions[0].value),
+  f: defaultFee,
+  s: parseInt(salaryOptions[0].value),
+  p: false,
+  fp: false,
+};
+
 export function getFinalFee({ fp: fastProcessing, f: fee }: ContractProps) {
-  return fastProcessing ? feeWithFastProcessing : fee;
+  return fastProcessing ? fastProcessingFee : defaultContractProps.f;
 }
 
 export function calculateContractCost(
