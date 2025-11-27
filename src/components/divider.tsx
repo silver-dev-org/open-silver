be60f73425ae96b9b3c9ae4afb1ce9c1c5b87ff4
Nicolás Pujia <nicopujia.dev@gmail.com>
2025-11-27 13:46:56 -0300

    fix divider

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -1,10 +1,17 @@
+import { cn } from "@/lib/utils";
 import Spacer, { spaceSizes } from "./spacer";
 
-export default function Divider() {
+export default function Divider({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
   return (
-    <div className={spaceSizes.sm.px}>
+    <div className={cn(spaceSizes.sm.px, "md:px-0")}>
       <Spacer size="lg" />
-      <div className="bg-foreground/25 h-px container mx-auto" />
+      <div
+        className={cn("bg-foreground h-px container mx-auto", className)}
+        {...props}
+      />
       <Spacer size="lg" />
     </div>
   );
