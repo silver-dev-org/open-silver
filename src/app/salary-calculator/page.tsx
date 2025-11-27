c5ce62f1787a59c7e82976e5901bc8d35acf0059
Nicolás Pujia <nicopujia.dev@gmail.com>
2025-11-27 10:18:41 -0300

    add promo card

--- src/app/salary-calculator/page.tsx
+++ src/app/salary-calculator/page.tsx
@@ -2,8 +2,18 @@ import Heading from "@/components/heading";
 import Section from "@/components/section";
 import Spacer from "@/components/spacer";
 import Description from "@/components/description";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Metadata } from "next";
+import Link from "next/link";
 import { SalaryCalculator } from "./client";
+import Divider from "@/components/divider";
 
 export const metadata: Metadata = {
   title: "Argentina Salary Calculator",
@@ -28,6 +38,36 @@ export default function Page() {
       </Description>
       <Spacer size="lg" />
       <SalaryCalculator />
+      <Divider />
+      <Card className="max-w-prose mx-auto text-balance">
+        <CardHeader>
+          <CardTitle>
+            Do you want to understand how hiring works in Argentina?
+          </CardTitle>
+          <CardDescription>
+            Book a meeting with{" "}
+            <Link
+              className="link"
+              target="_blank"
+              href="https://silver.dev/about"
+            >
+              our founder
+            </Link>{" "}
+            to learn about the details and find out the best strategy for your
+            company.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="justify-center">
+          <Button asChild size="lg" className="w-full">
+            <Link
+              target="_blank"
+              href="https://calendly.com/silver-dev/companies"
+            >
+              Book a Meeting
+            </Link>
+          </Button>
+        </CardFooter>
+      </Card>
     </Section>
   );
 }
