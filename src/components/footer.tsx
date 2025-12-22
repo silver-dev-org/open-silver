c2c6c3e566eb2379b6193b8bab816ed4fe9ccea3
Nicolás Pujia <nicopujia.dev@gmail.com>
2025-12-22 15:11:57 -0300

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,29 +9,68 @@ const links: {
   {
     group: "General",
     links: [
-      { href: "https://silver.dev", label: "Website" },
+      { href: "https://silver.dev/", label: "Home" },
       { href: "https://silver.dev/about", label: "About" },
       { href: "mailto:info@silver.dev", label: "Contact" },
+      { href: "https://silver.dev/careers", label: "Careers" },
+      { href: "https://silver.dev/intros", label: "Refer us" },
       {
         href: "https://github.com/silver-dev-org/open-silver",
         label: "Source code",
       },
+      { href: "https://silver.dev/dvd", label: "DVD" },
     ],
   },
   {
-    group: "For employers",
-    links: [{ href: "https://blog.silver.dev", label: "Hiring Blog" }],
+    group: "For Employers",
+    links: [
+      { href: "https://blog.silver.dev", label: "Hiring Blog" },
+      { href: "https://silver.dev/roles", label: "Roles we Support" },
+      { href: "https://silver.dev/placements", label: "Our Placements" },
+      { href: "https://silver.dev/aor", label: "Agency of Record" },
+      { href: "https://silver.dev/advisory", label: "Advisory" },
+      {
+        href: "https://silver.dev/argentina",
+        label: "Build your Argentina Hub",
+      },
+      {
+        href: "/salary-calculator",
+        label: "Argentina Salary Calculator",
+      },
+      { href: "https://silver.dev/agencies", label: "For Agencies" },
+    ],
   },
   {
-    group: "For talent",
+    group: "For Talent",
     links: [
-      { href: "https://ready.silver.dev/blog", label: "Career Blog" },
-      { href: "https://docs.silver.dev", label: "Silver Docs" },
-      { href: "https://silver.dev/podcast", label: "Podcast" },
-      { href: "https://silver.dev/ed", label: "SilverEd" },
-      { href: "https://silver.dev/community", label: "Community" },
       { href: "https://silver.dev/jobs", label: "Jobs" },
-      { href: "https://silver.dev/job-alerts", label: "New Job Alerts" },
+      { href: "https://app.silver.dev", label: "Platform" },
+      { href: "https://ready.silver.dev", label: "Interview Ready" },
+      { href: "https://silver.dev/referrals", label: "Refer Engineers" },
+      {
+        href: "https://wpm.silver.dev",
+        label: "WPM Game",
+      },
+      {
+        href: "/resume-checker",
+        label: "Resume Checker",
+      },
+      {
+        href: "/behavioral-checker",
+        label: "Behavioral Checker",
+      },
+      {
+        href: "/take-home-checker",
+        label: "Take-home Checker",
+      },
+      { href: "https://silver.dev/podcast", label: "Podcast" },
+      { href: "https://silver.dev/community", label: "Community" },
+      { href: "https://ready.silver.dev/blog", label: "Career Blog" },
+      { href: "https://silver.dev/library", label: "Recommended Books" },
+      { href: "https://docs.silver.dev", label: "Documentation" },
+      { href: "https://silver.dev/ed", label: "SilverEd" },
+      { href: "https://silver.dev/mentors", label: "Mentoring" },
+      { href: "https://silver.dev/english", label: "English Coaching" },
     ],
   },
 ];
@@ -52,7 +91,6 @@ export function Footer() {
                   <li key={href}>
                     <Link
                       href={href}
-                      target="_blank"
                       className={buttonVariants({ variant: "link" })}
                     >
                       {label}
